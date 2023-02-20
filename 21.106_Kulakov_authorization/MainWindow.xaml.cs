a3bb0f35470d0e13a1c9b76c62c385c69231c079
danii <daniil-kualkov@list.ru>
2023-02-20 17:01:24 +0700

    Добавьте файлы проекта.

---
+++ 21.106_Kulakov_authorization/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+
+namespace _21._106_Kulakov_authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //init ValidationUseCase with private setter
+        private readonly UseCases.ValidationUseCase validator;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Inject validateionUseCase
+            validator = new UseCases.ValidationUseCase();
+        }
+
+        //Funcition for requset in databese for auth user
+        private void auth(String userName, String password)
+        {
+            using (var context = new Dadabase.Entities() )
+            {
+                var user = context.User.FirstOrDefault(u => u.sername == userName && u.password == password);
+                var role = context.Role.FirstOrDefault(r => r.id == user.role);
+        
+                resultMessage(user, role);
+            }
+        }
+        //Message box for show auth result
+        private void resultMessage(Dadabase.User user, Dadabase.Role role)
+        {
+            if (user != null)
+            {
+                MessageBox.Show(LOGIN_SUCCES 
+                    + " \n Your surname is "
+                    + user.name + "\n Your role is " 
+                    + role.role_name, "Auth");
+            }else
+            {
+                MessageBox.Show(LOGIN_FAILED, "Auth");
+            }
+        }
+        private void btSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = edLOgin.Text.Trim();
+            var password = edPassword.Password.Trim();
+
+            //get auth useCase
+            var validationState = validator.ValidateData(login, password);
+
+            if (validationState.IsValid)
+            {
+               auth(login, password);
+            }
+            else
+            {
+                // Data is not valid, show error message
+                MessageBox.Show(string.Join("\n", validationState.Errors), "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        //Const info string for message box
+        public static string LOGIN_SUCCES = "Login Succes";
+        public static string LOGIN_FAILED = "Auth failed";
+        public static string AUTH_MESSAGE_TITLE = "Auth";
+
+    }
+}
---
+++ 21.106_Kulakov_authorization/UseCases/ValidationState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._106_Kulakov_authorization.UseCases
+{
+    public class ValidationState
+    {
+        public bool IsValid { get; set; }
+        public List<string> Errors { get; set; }
+
+        public ValidationState()
+        {
+            IsValid = true;
+            Errors = new List<string>();
+        }
+    }
+    public class ValidationUseCase
+    {
+        public ValidationState ValidateData(string name, string password)
+        {
+            var state = new ValidationState();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                state.IsValid = false;
+                state.Errors.Add("Name is required.");
+            }
+            if (password == null)
+            {
+                state.IsValid = false;
+                state.Errors.Add("Age must be at least 18 years old.");
+            }
+            if(password.Length <= 3)
+            {
+                state.IsValid = false;
+                state.Errors.Add("Password should be more then 4");
+            }
+            return state;
+        }
+    }
+}
